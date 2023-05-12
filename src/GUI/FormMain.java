d152946ac6aa8aaa7341e700dc9b56caf6c10587
Nguyên Khôi <khoile712003@gmail.com>
2023-05-13 01:33:19 +0700

    Add panelCustomer

--- src/GUI/FormMain.java
+++ src/GUI/FormMain.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class FormMain extends JFrame {
 	private JPanel contentPane;
-	private PanelBill panelBill = new PanelBill();
+	private PanelCustomer panelCustomer;
+	private JPanel pnMenu;
 	/**
 	 * Launch the application.
 	 */
@@ -31,20 +35,67 @@ public class FormMain extends JFrame {
 	 */
 	public FormMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 806, 590);
+		setBounds(100, 100, 861, 585);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		
 		contentPane.setLayout(null);
-		
-		JPanel pnMenu = new JPanel();
-		pnMenu.setBackground(new Color(128, 255, 255));
-		pnMenu.setBounds(0, 0, 190, 553);
-		contentPane.add(pnMenu);
-		pnMenu.setLayout(null);
-		
-		contentPane.add(panelBill);
 		setContentPane(contentPane);
+		
+		pnMenu = new JPanel();
+		pnMenu.setLayout(null);
+		pnMenu.setBackground(new Color(128, 255, 255));
+		pnMenu.setBounds(0, 0, 189, 553);
+		contentPane.add(pnMenu);
+		
+		
+		
+		panelCustomer = new PanelCustomer();
+		panelCustomer.setLayout(null);
+		panelCustomer.setBounds(189, 0, 671, 553);
+		contentPane.add(panelCustomer);
+		
+		
+		JButton btnEquipment = new JButton("EQUIPMENT");
+		btnEquipment.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnEquipment.setBackground(SystemColor.activeCaption);
+		btnEquipment.setBounds(0, 274, 190, 57);
+		pnMenu.add(btnEquipment);
+		
+		JButton btnCourse = new JButton("COURSE");
+		btnCourse.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnCourse.setBackground(SystemColor.activeCaption);
+		btnCourse.setBounds(0, 327, 190, 57);
+		pnMenu.add(btnCourse);
+		
+		JButton btnBill = new JButton("BILL");
+		btnBill.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnBill.setBackground(SystemColor.activeCaption);
+		btnBill.setBounds(0, 382, 190, 57);
+		pnMenu.add(btnBill);
+		
+		JButton btnUser = new JButton("USER");
+		btnUser.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnUser.setBackground(SystemColor.activeCaption);
+		btnUser.setBounds(0, 439, 190, 57);
+		pnMenu.add(btnUser);
+		
+		JButton btnSignOut = new JButton("SIGN OUT");
+		btnSignOut.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnSignOut.setBackground(SystemColor.activeCaption);
+		btnSignOut.setBounds(0, 496, 190, 57);
+		pnMenu.add(btnSignOut);
+		
+		JButton btnCustomer = new JButton("CUSTOMER");
+		btnCustomer.setFont(new Font("Yu Mincho Demibold", Font.PLAIN, 15));
+		btnCustomer.setBackground(SystemColor.activeCaption);
+		btnCustomer.setBounds(0, 219, 190, 57);
+		pnMenu.add(btnCustomer);
+		
+		JPanel panelAvatar = new JPanel();
+		panelAvatar.setBackground(Color.PINK);
+		panelAvatar.setBounds(0, 0, 190, 190);
+		pnMenu.add(panelAvatar);
+		
 	}
-
 }
